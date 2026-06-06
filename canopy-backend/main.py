a4c0f64102ad724a2c97617a8822a265bcfacd1d
Zikra Shaikh <zikraimtiyazshaikh@gmail.com>
2026-06-06 23:18:35 +0530

    Allow all CORS origins for production deployment

--- canopy-backend/main.py
+++ canopy-backend/main.py
@@ -9,8 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
